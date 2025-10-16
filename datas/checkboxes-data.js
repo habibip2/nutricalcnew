1e54805cc774f6f10f93973fe9c62fb58f82b39d
habibip2 <134698632+habibip2@users.noreply.github.com>
2025-10-16 07:06:45 +0600

    Add files via upload

---
+++ datas/checkboxes-data.js
@@ -0,0 +1,122 @@
+const checkboxDatas = [
+  {
+    zat: 'Energi',
+    className: 'energyResult',
+    headingName: 'Energi (kkal)',
+    totalId: 'totalEnergy'
+  },
+  {
+    zat: 'Protein',
+    className: 'proteinResult',
+    headingName: 'Protein (g)',
+    totalId: 'totalProtein'
+  },
+  {
+    zat: 'Lemak',
+    className: 'fatResult',
+    headingName: 'Lemak (g)',
+    totalId: 'totalFat'
+  },
+  {
+    zat: 'Karbohidrat',
+    className: 'carbsResult',
+    headingName: 'Karbohidrat (g)',
+    totalId: 'totalCarbs'
+  },
+  {
+    zat: 'Serat',
+    className: 'fiberResult',
+    headingName: 'Serat (g)',
+    totalId: 'totalFiber'
+  },
+  {
+    zat: 'Abu',
+    className: 'CarbonResult',
+    headingName: 'Abu (g)',
+    totalId: 'totalCarbon'
+  },
+  {
+    zat: 'Kalsium',
+    className: 'CalciumResult',
+    headingName: 'Kalsium (mg)',
+    totalId: 'totalCalcium'
+  },
+  {
+    zat: 'Fosfor',
+    className: 'PhosporResult',
+    headingName: 'Fosfor (mg)',
+    totalId: 'totalPhospor'
+  },
+  {
+    zat: 'Zat Besi',
+    className: 'IronResult',
+    headingName: 'Besi (mg)',
+    totalId: 'totalIron'
+  },
+  {
+    zat: 'Natrium',
+    className: 'SodiumResult',
+    headingName: 'Natrium (mg)',
+    totalId: 'totalSodium'
+  },
+  {
+    zat: 'Kalium',
+    className: 'PotassiumResult',
+    headingName: 'Kalium (mg)',
+    totalId: 'totalPotassium'
+  },
+  {
+    zat: 'Tembaga',
+    className: 'CuprumResult',
+    headingName: 'Tembaga (mg)',
+    totalId: 'totalCuprum'
+  },
+  {
+    zat: 'Seng',
+    className: 'ZincResult',
+    headingName: 'Zinc (mg)',
+    totalId: 'totalZinc'
+  },
+  {
+    zat: 'Retinol',
+    className: 'RetinolResult',
+    headingName: 'Retinol (mcg)',
+    totalId: 'totalRetinol'
+  },
+  {
+    zat: 'Betakaroten',
+    className: 'BKarResult',
+    headingName: 'Betakaroten (mcg)',
+    totalId: 'totalBKar'
+  },
+  {
+    zat: 'Kartotal',
+    className: 'KarTotResult',
+    headingName: 'KarTotal (mcg)',
+    totalId: 'totalKarTot'
+  },
+  {
+    zat: 'Thamin',
+    className: 'ThaminResult',
+    headingName: 'Thamin (mg)',
+    totalId: 'totalThamin'
+  },
+  {
+    zat: 'Riboflavin',
+    className: 'RiboflavinResult',
+    headingName: 'Riboflavin (mg)',
+    totalId: 'totalRiboflavin'
+  },
+  {
+    zat: 'Niasin',
+    className: 'NiasinResult',
+    headingName: 'Niasin (mg)',
+    totalId: 'totalNiasin'
+  },
+  {
+    zat: 'Vitamin C',
+    className: 'VitaminCResult',
+    headingName: 'Vitamin C (mg)',
+    totalId: 'totalVitaminC'
+  }
+];
